7a8af8162cf0b26eac357585660c972a5bb76c7e
Satya prakash sahu <sahusatyaprakash83@gmail.com>
2023-05-09 14:42:09 +0530

    added gitignore

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+
+<body>
+    <main>
+        <form action="login.php" method="post">
+            <div class="input-field">
+                <label for="username">User Name</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="input-field">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </form>
+    </main>
+</body>
+
+</html>
