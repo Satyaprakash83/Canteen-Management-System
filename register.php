ba3eea3940da5aca5048c82540b516d9b207624c
Satya prakash sahu <sahusatyaprakash83@gmail.com>
2023-05-10 20:52:31 +0530

    added functionality in apply leave page

--- register.php
+++ register.php
@@ -5,21 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
+    <title>Document</title>
 </head>
 
 <body>
     <main>
-        <form action="login.php" method="post">
+        <form action="register.php" method="post">
             <div class="input-field">
                 <label for="username">User Name</label>
-                <input type="text" name="username" id="username">
+                <input type="text" name="username" id="username" placeholder="User Name">
             </div>
             <div class="input-field">
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" placeholder="">
             </div>
-
+            <div class="input-field">
+                <label for="password">Password</label>
+                <input type="password" name="conformPasswors" id="conformPasswors">
+            </div>
         </form>
     </main>
 </body>
